7d1c70b3c22f31e79b8695e7a554221b6654695b
unknown <diyasingh83693@gmail.com>
2025-08-17 19:48:40 +0530

    routes updated

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { analyzeWebsite, deleteWebsite, getWebsites, updateWebsite } from './controller/websiteController.js';
 const router = express.Router();
 router.post("/analyze", analyzeWebsite);
-router.get("/websites/", getWebsites);
+router.get("/websites", getWebsites);
 router.put("/websites/:id", updateWebsite);
 router.delete("/websites/:id", deleteWebsite);
 
