4c9cd6525536b163712d174b121f75c404fd65cb
unknown <diyasingh83693@gmail.com>
2025-08-17 19:57:23 +0530

    installed puppeteer-core for vercel

--- src/controller/websiteController.js
+++ src/controller/websiteController.js
@@ -1,7 +1,7 @@
 import pool from "../db.js";
 import * as cheerio from "cheerio";
-import puppeteer from "puppeteer";
-import axios from "axios";
+import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 import { generateSummary } from "../../services/openaiService.js";
 
 //scrape with cheerio
@@ -30,16 +30,23 @@ async function scrapeWithCheerio(urlOrHtml, isHtml = false) {
 
 //scrape with puppeteer
 async function scrapeWithPuppeteer(url) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
+
   const page = await browser.newPage();
   await page.setExtraHTTPHeaders({
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/115.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0",
+    "Accept-Language": "en-US,en;q=0.9"
   });
+
   await page.goto(url, { waitUntil: "domcontentloaded", timeout: 30000 });
   const content = await page.content();
   await browser.close();
-  return scrapeWithCheerio(content);
+  return scrapeWithCheerio(content, true);
 }
 
 // analyze website
@@ -47,11 +54,16 @@ export async function analyzeWebsite(req, res) {
 
   try {
     const { url } = req.body;
-    const websiteContent = await scrapeWithCheerio(url);
+    let websiteContent;
+    try {
+      websiteContent = await scrapeWithPuppeteer(url);
+    } catch {
+      websiteContent = await scrapeWithCheerio(url);
+    }
     const aiSummary = await generateSummary(websiteContent.description);
 
     const result = await pool.query("INSERT INTO websites (url, brand,description, aiSummary) VALUES ($1, $2, $3, $4)RETURNING *",
-        [url, websiteContent.brand, websiteContent.description, aiSummary]
+      [url, websiteContent.brand, websiteContent.description, aiSummary]
     );
 
     res.json(result.rows[0]);
@@ -83,7 +95,9 @@ export async function updateWebsite(req, res) {
        WHERE id = $3 RETURNING *;`,
       [brand, description, id]
     );
-    if (rows.length === 0) return res.status(404).json({ error: "Website not found" });
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Website not found" })
+    };
     res.json(rows[0]);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -96,7 +110,9 @@ export async function deleteWebsite(req, res) {
 
   try {
     const { rowCount } = await pool.query(`DELETE FROM websites WHERE id = $1;`, [id]);
-    if (rowCount === 0) return res.status(404).json({ error: "Website not found" });
+    if (rowCount === 0) {
+      return res.status(404).json({ error: "Website not found" });
+    }
     res.json({ message: "Website deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
